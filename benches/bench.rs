25a8d25bce685a79d49088c2bbfc6a9495ea148e
doomed-neko <79870712+doomed-neko@users.noreply.github.com>
2026-08-20 01:26:07 +0300

    add MOAR benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,13 +1,21 @@
 use criterion::{Criterion, criterion_group, criterion_main};
-use gol::Game;
+use gol::{COLS, Game, ROWS};
 use rand::random_bool;
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let mut buf = vec![false; 10000];
+fn bench_10k(c: &mut Criterion) {
+    let (cols, rows) = (100, 100);
+    let mut buf = vec![false; rows * cols];
     buf.fill_with(|| random_bool(0.05));
-    let mut game = Game::new(buf, 100, 100);
-    c.bench_function("game", |b| b.iter(|| game.next_gen()));
+    let mut game = Game::new(buf, cols, rows);
+    c.bench_function("10k", |b| b.iter(|| game.next_gen()));
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn bench_cols_by_rows(c: &mut Criterion) {
+    let mut buf = vec![false; COLS * ROWS];
+    buf.fill_with(|| random_bool(0.05));
+    let mut game = Game::new(buf, COLS, ROWS);
+    c.bench_function("ROWS*COLS", |b| b.iter(|| game.next_gen()));
+}
+
+criterion_group!(benches, bench_10k, bench_cols_by_rows);
 criterion_main!(benches);
