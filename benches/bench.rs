c45f4b207f1bde50e3dbdb1f816e2c1b3c3b0230
doomed-neko <79870712+doomed-neko@users.noreply.github.com>
2026-08-21 04:10:47 +0300

    add more benches

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,12 +3,13 @@ use gol::game::Game;
 
 fn bench_1k(c: &mut Criterion) {
     let mut game = Game::new_random(100, 10, 0.3);
-    c.bench_function("ROWS*COLS", |b| b.iter(|| game.next_gen()));
+    c.bench_function("1k", |b| b.iter(|| game.next_gen()));
 }
 
 fn bench_5k(c: &mut Criterion) {
-    let mut game = Game::new_random(100, 10, 0.3);
-    c.bench_function("ROWS*COLS", |b| b.iter(|| game.next_gen()));
+    let (cols, rows) = (100, 50);
+    let mut game = Game::new_random(cols, rows, 0.3);
+    c.bench_function("5k", |b| b.iter(|| game.next_gen()));
 }
 
 fn bench_10k(c: &mut Criterion) {
@@ -17,5 +18,37 @@ fn bench_10k(c: &mut Criterion) {
     c.bench_function("10k", |b| b.iter(|| game.next_gen()));
 }
 
-criterion_group!(benches, bench_1k, bench_5k, bench_10k);
+fn bench_50k(c: &mut Criterion) {
+    let (cols, rows) = (100, 500);
+    let mut game = Game::new_random(cols, rows, 0.3);
+    c.bench_function("50", |b| b.iter(|| game.next_gen()));
+}
+
+fn bench_100k(c: &mut Criterion) {
+    let (cols, rows) = (1000, 100);
+    let mut game = Game::new_random(cols, rows, 0.3);
+    c.bench_function("100k", |b| b.iter(|| game.next_gen()));
+}
+
+fn bench_500k(c: &mut Criterion) {
+    let (cols, rows) = (1000, 500);
+    let mut game = Game::new_random(cols, rows, 0.3);
+    c.bench_function("500k", |b| b.iter(|| game.next_gen()));
+}
+
+fn bench_1m(c: &mut Criterion) {
+    let (cols, rows) = (1000, 1000);
+    let mut game = Game::new_random(cols, rows, 0.3);
+    c.bench_function("1m", |b| b.iter(|| game.next_gen()));
+}
+
+fn bench_5m(c: &mut Criterion) {
+    let (cols, rows) = (1000, 5000);
+    let mut game = Game::new_random(cols, rows, 0.3);
+    c.bench_function("5m", |b| b.iter(|| game.next_gen()));
+}
+
+criterion_group!(
+    benches, bench_1k, bench_5k, bench_10k, bench_50k, bench_100k, bench_500k, bench_1m, bench_5m
+);
 criterion_main!(benches);
